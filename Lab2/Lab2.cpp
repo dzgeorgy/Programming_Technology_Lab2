b85cf3c25b497edb361609559eb78fa87987d60f
Georgy Dzutsev <15524628+dzgeorgy@users.noreply.github.com>
2020-12-20 22:02:18 +0300

    Fixing issue #4 [Reading from Console] (#5)
    * Refactor main functionality to match C++/CLR
    
    * Removed parameter of size of array in print_array function cause we can iterate with foreach loop
    
    * Removed parameter of size of array in print_array function cause we can iterate with foreach loop
    
    * Handle application crash on incorrect input value

--- Lab2/Lab2.cpp
+++ Lab2/Lab2.cpp
@@ -4,17 +4,19 @@ using namespace System;
 
 int main(array<String ^> ^args)
 {
-	const auto rows = 3;
+    Console::WriteLine(L"This application calculates two arrays from matrices by selecting max value from each column.\n");
+	  const auto rows = 3;
     const auto columns = 6;
     const auto matrix_a = create_matrix(rows, columns);
     const auto matrix_b = create_matrix(rows, columns);
+    system("cls");
     Console::WriteLine(L"Matrix A is");
     print_matrix(matrix_a, rows, columns);
     Console::WriteLine(L"Matrix B is");
     print_matrix(matrix_b, rows, columns);
     const auto array_c = find_max_elements_in_column(matrix_a, rows, columns);
     const auto array_d = find_max_elements_in_column(matrix_b, rows, columns);
-    Console::WriteLine(L"Array C is");
+    Console::WriteLine(L"\nArray C is");
     print_array(array_c);
     Console::WriteLine(L"\nArray D is");
     print_array(array_d);
@@ -23,10 +25,26 @@ int main(array<String ^> ^args)
 array<int, 2>^ create_matrix(const int rows, const int columns)
 {
     const auto matrix = gcnew array<int, 2>(rows, columns);
-    Console::WriteLine(L"Please fill a matrix with size {0}x{1}:", rows, columns);
+    Console::WriteLine(L"Please fill a matrix with size {0}x{1}: ", rows, columns);
     for (auto i = 0; i < rows; ++i)
         for (auto j = 0; j < columns; ++j)
-            matrix[i,j] = Convert::ToInt32(Console::ReadLine());
+        {
+            bool is_correct;
+            do
+            {
+                is_correct = true;
+                try
+                {
+                    Console::Write(L"Element {0}x{1}:", i + 1, j + 1);
+                    matrix[i, j] = Convert::ToInt32(Console::ReadLine());
+                }
+                catch (FormatException^ exception)
+                {
+                    is_correct = false;
+                    Console::WriteLine(L"You entered incorrect value!\nPlease try again...");
+                }
+            } while (!is_correct);
+        }
     return matrix;
 }
 
